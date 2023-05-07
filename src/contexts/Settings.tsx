76a743a85b5eaa0b69246eb2ebcfcbf9642cfb29
grum-tez <grum.tez@gmail.com>
2023-05-07 14:07:58 +0000

    added contexts, adjusted taquito version to work

---
+++ src/contexts/Settings.tsx
@@ -0,0 +1,25 @@
+import { NetworkType } from '@airgap/beacon-sdk'
+import constate from 'constate'
+import { useState } from 'react'
+
+export const [
+  SettingsProvider,
+  useAppName,
+  useEndpoint,
+  useNetwork,
+  useContractAddress,
+] = constate(
+  () => {
+    const [settingsState] = useState({
+      app_name: 'My DApp',
+      endpoint: 'https://ghostnet.tezos.marigold.dev',
+      network: NetworkType.GHOSTNET,
+      contract: 'KT1UtJR46i6pEVLeLx7SMB5Gf5Q9L2y1xiV4',
+    })
+    return settingsState
+  },
+  (v) => v.app_name,
+  (v) => v.endpoint,
+  (v) => v.network,
+  (v) => v.contract
+)
