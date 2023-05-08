13de8ae1bd3a1342f64d755df37662f70c38d9f4
grum-tez <grum.tez@gmail.com>
2023-05-08 21:16:02 +1200

    page working - beacon bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
 import { useState } from 'react'
+import { useTezosToolkit } from "./contexts/Taquito"
+import { useContractAddress } from "./contexts/Settings"
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const ttk = useTezosToolkit()
+  const contractAddress = useContractAddress()
+  const [actualContract, setActualContract] = React.useState(undefined)
+  const [isActualContract, setIsActualContract] = React.useState(true)
+  const [isAddEntrypoint, setIsAddEntrypoint] = React.useState(true)
+  const [loadingContract, setLoadingContract] = React.useState(true)
+
+  React.useEffect(() => {
+    console.log('mounting')
+    let ignore = false
+
+    async function startFetching() {
+      try {
+        const ctr = await (ttk as any).wallet.at(contractAddress)
+        if (!ignore) {
+          setActualContract(ctr)
+          setIsActualContract(true)
+          setIsAddEntrypoint(!!(ctr as any)?.entrypoints?.entrypoints?.add_poll)
+        }
+      } catch (e) {
+        setIsActualContract(false)
+        setIsAddEntrypoint(false)
+        console.log(e)
+      }
+      setLoadingContract(false)
+    }
+
+    startFetching()
+    return () => {
+      console.log('unmounting')
+      ignore = true
+    }
+  }, [])
+
+  console.log('Contract found: ', isActualContract)
+  console.log('Add entrypoint found: ', isAddEntrypoint)
+  console.log(actualContract)
+  console.log((actualContract as any)?.entrypoints?.entrypoints?.add_poll)
 
   return (
     <>
--- src/contexts/Contract.tsx
+++ src/contexts/Contract.tsx
@@ -8,12 +8,12 @@ import { useTezosToolkit } from './Taquito'
 
 export const [ContractProvider, useContract] = constate(
   () => {
-    const tezos = useTezosToolkit()
+    const ttk = useTezosToolkit()
     const address = useContractAddress()
     const [contract] = useState({
       contract: new Contract(address),
     })
-    set_binder_tezos_toolkit(tezos)
+    set_binder_tezos_toolkit(ttk)
     return contract
   },
   (v) => v.contract
